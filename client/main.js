f5932c491eb28be793805519436e06a7b5cd02eb
Oleh Onyshchak <oleh.onyshchak@gmail.com>
2018-05-11 16:31:45 +0300

    Implemented client

---
+++ client/main.js
@@ -0,0 +1,68 @@
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
+
+
+function onProcess() {
+    let dataSource = getDataSource();
+
+    let req = `http://localhost:3000/weather${dataSource}`;
+    getJSON(req, (err, data) => {
+        if (err !== null) {
+            alert('Something went wrong: ' + err);
+        } else {
+            show(data);
+        }
+    });
+}
+
+function getDataSource() {
+    var e = document.getElementById("dataSource");
+    return e.options[e.selectedIndex].value;
+}
+
+function show(arr) {
+    clearTable();
+    let forecast = arr.daily;
+
+    for (let i = 0; i < forecast.length; ++i) {
+        let obj = forecast[i];
+        addRow([obj.date, obj.min_temp, obj.max_temp]);
+    }
+}
+
+function clearTable() {
+    var table = document.getElementById("dataTable");
+
+    for(let i = table.rows.length - 1; i > 0; i--)
+    {
+        table.deleteRow(i);
+    }
+}
+
+function addRow(vals) {
+    let table = document.getElementById("dataTable");
+
+    var tr = document.createElement("tr");
+
+    for (let i = 0; i < vals.length; ++i) {
+        var td = document.createElement("td");
+        var txt = document.createTextNode(vals[i]);
+
+        td.appendChild(txt);
+        tr.appendChild(td);
+    }
+
+    table.appendChild(tr);
+}
--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 `use strict`;
 const express = require('express');
 const app = express();
+var cors = require('cors');
+
+app.use(cors());
 
 const dataSource = require(`./data-source`);
 
@@ -34,14 +37,13 @@ app.get('/weather/openWeather', async (req, res, next) => {
     }
 });
 
-app.get('/weather', async (req, res, next) => {
-    try {
-        let result = await dataSource.forecast(dataSource.dataSource.composite);
-        res.status(200).json(result);
-    }
-    catch (e) {
-        next(e);
-    }
+app.get('/weather', (req, res, next) => {
+    dataSource.forecast(dataSource.dataSource.composite)
+    .then(data => {
+        res.status(200);
+        res.send(data);
+    })
+    .catch(next);
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
